b58017145910728c5364f6204ec9362be34d70da
Russ <sayersr@amazon.com>
2022-01-12 14:18:43 +1100

    gradle fixes / linting

--- listDragons/src/main/java/com/mycompany/app/ListDragons.java
+++ listDragons/src/main/java/com/mycompany/app/ListDragons.java
@@ -18,7 +18,6 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
